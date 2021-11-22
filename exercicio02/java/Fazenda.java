ed8419c0f07d15206e0c068740dab74dab33fac2
geison36 <87587094+geison36@users.noreply.github.com>
2021-11-22 00:33:33 -0300

    Update Fazenda.java

--- exercicio02/java/Fazenda.java
+++ exercicio02/java/Fazenda.java
@@ -1,13 +1,19 @@
 package exercicio02.java;
 
 public class Fazenda {
-    String nome;
-    double area;
-    String propietario;
+    public String nome;
+    public double area;
+    public String propietario;
 
     public Fazenda(String nome, double area, String propietario) {
         this.nome = nome;
         this.area = area;
         this.propietario = propietario;
     }
-}
+    public void imprimi() {
+        System.out.println("Fazenda: " + this.nome);
+        System.out.println("Area: " + this.area + "a");
+        System.out.println("Proprietario: + this.proprietario);
+                           
+      }
+    }
