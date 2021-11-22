bb37d079f1d628e13d757c49458182217e9a68e7
geison36 <87587094+geison36@users.noreply.github.com>
2021-11-22 00:53:00 -0300

    Create main.java

---
+++ exercicio02/java/main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    Figura f_01 = new Figura(-2, 5); //instancia_Figura
+    f_01.imprimi();
+    Fazenda fzd = new Fazenda("Haras Manga_Larga", 2000, "Fábio");  //instancia_Fazenda
+    fzd.imprimi(); 
+  }
+}
