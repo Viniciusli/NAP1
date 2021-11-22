01a64b7dd4d8f396c0c3cd9da40ce74b93a920f0
geison36 <87587094+geison36@users.noreply.github.com>
2021-11-22 01:04:06 -0300

    Update main.java

--- exercicio02/java/main.java
+++ exercicio02/java/main.java
@@ -1,8 +1,9 @@
 class Main {
   public static void main(String[] args) {
     Figura f_01 = new Figura(-2, 5); //instancia_Figura
-    f_01.imprimi();
+    f_01.imprimi(); //método de impressão   
     Fazenda fzd = new Fazenda("Haras Manga_Larga", 2000, "Fábio");  //instancia_Fazenda
     fzd.imprimi(); 
+                  /*onderem solicitada na atividade "nome, area e proprietario" */
   }
 }
