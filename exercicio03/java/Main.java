b9a118806e1216c3d4b55a5da8083d9df0b2efbd
geison36 <87587094+geison36@users.noreply.github.com>
2021-11-23 21:36:40 -0300

    Update Main.java

--- exercicio03/java/Main.java
+++ exercicio03/java/Main.java
@@ -1,14 +1,18 @@
-package exercicio03.java;
+package atividad_ex03;
+public class Atividad_Ex03 {
+        public static void main(String[] args) {
+    Figura f_01 = new Figura(2, 5); // instancia_Figura
 
-public class Main {
-    public static void main(String[] args) {
-        /**
-         * Figura f_01 = new Figura(-2, 5); // instancia_Figura f_01.imprimi(); //
-         * método de impressão
-         **/
-        Fazenda2 fazenda = new Fazenda2("Haras Manga_Larga", 2000, "Fábio", "batata");
-        // instancia_Fazenda
-        fazenda.imprimi();
-        /* onderem solicitada na atividade "nome, area e proprietario" */
-    }
+    f_01.desenhar();
+    f_01.pintar();
+    f_01.imprimi();
+
+    System.out.println("______________________________________________________\n"); 
+
+     Fazenda fzd = new Fazenda("Haras Manga_Larga", 2000 , "Fábio"); // instancia_Fazenda
+    
+    fzd.imprimi();
+    fzd.plantar();
+    fzd.adubar();    
+  }
 }
