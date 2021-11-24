2df6becf9d065afd82144b53461419d3c71f5994
geison36 <87587094+geison36@users.noreply.github.com>
2021-11-24 01:02:24 -0300

    Update main.py

--- exercicio04/python/main.py
+++ exercicio04/python/main.py
@@ -1,1 +1,17 @@
+# instanciando Figura
+f_01 = Figura( 2, 5)
 
+f_01.imprimi()
+f_01.quadrante()
+
+print(\n)
+
+f_01.mover()
+
+f_01.imprimi()
+f_01.quadrante()
+
+f_01.mover()
+
+f_01.imprimi()
+f_01.quadrante()
