6dd357fc2291f849643af248eebc7172880da347
geison36 <87587094+geison36@users.noreply.github.com>
2021-11-24 01:11:48 -0300

    Update main.py

--- exercicio04/python/main.py
+++ exercicio04/python/main.py
@@ -1,13 +1,11 @@
 # instanciando Figura
 f_01 = Figura( 2, 5)
 
-f_01.imprimi()
+f_01.get_coodenadas()
 f_01.quadrante()
 
-print(\n)
-
 f_01.mover()
-
+f_01.get_coodenadas()
 f_01.imprimi()
 f_01.quadrante()
 
