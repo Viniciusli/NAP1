a0b4aa5dcd591bddebfb04ef086d658ff75abfba
Viniciusli <viniciusleeg@outlook.com>
2021-11-24 21:57:29 -0300

    fixing the package and distance method

--- exercicio05/Java/Ponto.java
+++ exercicio05/Java/Ponto.java
@@ -1,45 +1,45 @@
-package exercicio05.java;
+package exercicio05.Java;
 
-public class Ponto{
-  
-public double coordx;
-public double coordy;
+public class Ponto {
 
-public Ponto(double coordx, double coordy){
+  public double coordx;
+  public double coordy;
+
+  public Ponto(double coordx, double coordy) {
     this.coordx = coordx;
     this.coordy = coordy;
   }
-  //não havia na atividade, mas usamos o metodo imprimir.
-public void imprimir (){
+
+  // não havia na atividade, mas usamos o metodo imprimir.
+  public void imprimir() {
     System.out.println("As coordenadas de X e de Y no ponto A são de : " + this.getcoordx() + " e " + this.getcoordy());
 
-}
+  }
 
-public double getcoordx() {
-   return this.coordx;
+  public double getcoordx() {
+    return this.coordx;
   }
 
   public double getcoordy() {
-   return this.coordy;
+    return this.coordy;
   }
 
-public void setcoordx(double Coordx) {
-   this.coordx = Coordx;
+  public void setcoordx(double Coordx) {
+    this.coordx = Coordx;
   }
 
-public void setcoordy(double Coordy) {
-   this.coordy = Coordy;
+  public void setcoordy(double Coordy) {
+    this.coordy = Coordy;
   }
 
-
-public double distancia(double coordx, double coordy){
-   return Math.sqrt(Math.pow(this.coordx - coordx, 2) + Math.pow(coordy - coordy, 2));
+  public double distancia(double coordx, double coordy) {
+    return Math.sqrt(Math.pow(this.coordx - coordx, 2) + Math.pow(this.coordy - coordy, 2));
   }
-   
-public double distancia(Ponto a){
-  return Math.sqrt(Math.pow(this.coordx - a.getcoordx(),2) + Math.pow(this.coordy - a.getcoordy(), 2));
 
-  // o "a" repressenta o Ponto.
- }
+  public double distancia(Ponto a) {
+    return Math.sqrt(Math.pow(this.coordx - a.getcoordx(), 2) + Math.pow(this.coordy - a.getcoordy(), 2));
+
+    // o "a" repressenta o Ponto.
+  }
 
 }
